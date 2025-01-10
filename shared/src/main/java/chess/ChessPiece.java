fc28b3341e681148c74e6033be0504f47d161ff4
mburov99 <merlinmax991@gmail.com>
2025-01-10 11:32:23 -0700

    modified ChessBoard methods and added logic code for rook movement

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List; // imported recently
 
 /**
  * Represents a single chess piece
@@ -11,7 +12,12 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -30,14 +36,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -48,6 +54,80 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        if (this.type == PieceType.ROOK) {
+            //call rook move method in Rook class
+        } else if (this.type == PieceType.BISHOP) {
+            //call bishop move method in bishop class
+        }
         return new ArrayList<>();
+    }
+
+    /* Bishop Piece */
+    public static class Bishop extends ChessPiece {
+        public Bishop(ChessGame.TeamColor pieceColor) {
+            super(pieceColor, PieceType.BISHOP);
+        }
+
+        @Override
+        public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+
+            return new ArrayList<>();
+
+            //return java.util.List.of();
+        }
+    }
+
+    /* Rook Piece */
+    public static class Rook extends ChessPiece {
+        public Rook(ChessGame.TeamColor pieceColor) {
+            super(pieceColor, PieceType.ROOK);
+        }
+
+        @Override
+        public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+
+            List<ChessMove> possibleMoves = new ArrayList<>();
+//            int[] rowDir = {-1, 1, 0, 0}; // Up, Down
+//            int[] colDir = {0, 0, -1, 1}; // Left, Right
+
+
+            for (int row = myPosition.getRow(); row <= 8; ++row) {
+                ChessPosition position = new ChessPosition(row, myPosition.getColumn());
+                ChessPiece piece = board.getPiece(position);
+                if(piece == null) {
+                    ChessMove move = new ChessMove(myPosition, position, null);
+                    possibleMoves.add(move);
+                } else {
+                    if (this.getTeamColor() != piece.getTeamColor()) {
+                        ChessMove move = new ChessMove(myPosition, position, null);
+                        possibleMoves.add(move);
+                    }
+                    break;
+                }
+            }
+
+            //repeat for column.
+            for (int col = myPosition.getColumn(); col <= 8; ++col) {
+                ChessPosition position = new ChessPosition(myPosition.getRow(), col);
+                ChessPiece piece = board.getPiece(position);
+                if(piece == null) {
+                    ChessMove move = new ChessMove(myPosition, position, null);
+                    possibleMoves.add(move);
+                } else {
+                    if (this.getTeamColor() != piece.getTeamColor()) {
+                        ChessMove move = new ChessMove(myPosition, position, null);
+                        possibleMoves.add(move);
+                    }
+                    break;
+                }
+            }
+
+
+
+            //check if out of bounds
+
+            //check is square is empty or can capture?
+            return possibleMoves;
+        }
     }
 }
